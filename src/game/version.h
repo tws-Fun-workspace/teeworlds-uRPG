f98fb62badf977a76dc18173a4ade17ee38c6656
XXLTomate <XXLTomate@v93.eu>
2011-04-28 12:15:16 +0200

    Increased version

--- src/game/version.h
+++ src/game/version.h
@@ -5,5 +5,5 @@
 #include "generated/nethash.c"
 #define GAME_VERSION "0.6 trunk, 1.075a"
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
-#define XXL_VERSION "1.6"
+#define XXL_VERSION "1.6a"
 #endif
