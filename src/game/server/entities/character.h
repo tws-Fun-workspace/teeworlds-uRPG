1ec8926609c7c1e1d3e7d3f1b68a2524b3a3ca74
fisted <van.fisted@googlemail.com>
2011-05-15 20:49:35 +0200

    added getter for remaining freeze ticks

--- src/game/server/entities/character.h
+++ src/game/server/entities/character.h
@@ -67,6 +67,7 @@ public:
 	class CPlayer *GetPlayer() { return m_pPlayer; }
 
 	void Freeze(int Ticks);
+	int GetFreezeTicks();
 private:
 	// player controlling this character
 	class CPlayer *m_pPlayer;
