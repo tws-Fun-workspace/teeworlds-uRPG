95e50ab2c93a32e6b82ddc6c0332eaf31c1f09fc
XXLTomate <XXLTomate@v93.eu>
2011-06-28 15:51:34 +0200

    another rescue fix

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -1861,7 +1861,8 @@ void CCharacter::HandleRainbow()
 
 void CCharacter::HandleRescue()
 { 	//Do I realy need everything?
-	if(IsGrounded() &&  m_TileIndex != TILE_FREEZE && m_TileFIndex != TILE_FREEZE && !m_FreezeTime && !m_DeepFreeze && !m_LastRescueSave && m_Pos != m_RescuePos && m_Pos != vec2(0,0)){
+	if(IsGrounded() &&  m_TileIndex != TILE_FREEZE && m_TileFIndex != TILE_FREEZE && !m_FreezeTime && !m_DeepFreeze && !m_LastRescueSave &&  m_Pos == m_PrevPos)
+	{
 		m_RescuePos = m_Pos;
 		m_LastRescueSave = 7; //not every point will be stored
 	}
