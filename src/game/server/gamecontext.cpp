b16e00baa414027ac13f5951606295b726c87bf3
eeeee <eeeee@qwe123.info>
2011-08-15 02:00:23 +0200

    anti-rainbow patch

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -1174,7 +1174,11 @@ void CGameContext::OnMessage(int MsgID, CUnpacker *pUnpacker, int ClientID)
 	else if (MsgID == NETMSGTYPE_CL_CHANGEINFO)
 	{
 		if(g_Config.m_SvSpamprotection && pPlayer->m_LastChangeInfo && pPlayer->m_LastChangeInfo+Server()->TickSpeed()*g_Config.m_SvInfoChangeDelay > Server()->Tick())
+		{
+			if (pPlayer->m_LastChangeInfo < Server()->Tick())
+				pPlayer->m_LastChangeInfo += Server()->TickSpeed()/2;
 			return;
+		}
 
 		CNetMsg_Cl_ChangeInfo *pMsg = (CNetMsg_Cl_ChangeInfo *)pRawMsg;
 		pPlayer->m_LastChangeInfo = Server()->Tick();
