46ae32d33245659e6bed269d74100d4dd8a1838d
eeeee <eeeee@qwe123.info>
2011-11-27 15:27:14 +0100

    fix msvc build

--- src/game/server/gamecontext.h
+++ src/game/server/gamecontext.h
@@ -15,8 +15,14 @@
 #include "gameworld.h"
 #include "player.h"
 
+#ifdef _MSC_VER
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
+#else
 #include <stdint.h>
-
+#endif
 /*
 	Tick
 		Game Context (CGameContext::tick)
