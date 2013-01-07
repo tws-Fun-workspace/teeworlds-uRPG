18909e55273dc4050c6750fcf63ec455fa74c4f6
eeeee <eeeee@qwe123.info>
2013-01-07 11:03:49 +0400

    bam fix

--- bam.lua
+++ bam.lua
@@ -21,13 +21,13 @@ function Script(name)
 	return "python " .. name
 end
 
-function CHash(output, ...)
+function CHash(output, defname, ...)
 	local inputs = TableFlatten({...})
 
 	output = Path(output)
 
 	-- compile all the files
-	local cmd = Script("scripts/cmd5.py") .. " "
+	local cmd = Script("scripts/cmd5.py") .. " " .. defname .. " "
 	for index, inname in ipairs(inputs) do
 		cmd = cmd .. Path(inname) .. " "
 	end
@@ -106,7 +106,8 @@ AddDependency(network_source, network_header)
 AddDependency(client_content_source, client_content_header)
 AddDependency(server_content_source, server_content_header)
 
-nethash = CHash("src/game/generated/nethash.cpp", "src/engine/shared/protocol.h", "src/game/generated/protocol.h", "src/game/tuning.h", "src/game/gamecore.cpp", network_header)
+nethash = CHash("src/game/generated/nethash.cpp", "GAME_NETVERSION_HASH", "src/engine/shared/protocol.h", "src/game/generated/protocol.h", "src/game/tuning.h", "src/game/gamecore.cpp", network_header)
+custnethash = CHash("src/game/generated/nethash_cust.c", "GAME_NETVERSION_HASH_CUST", "src/engine/shared/protocol.h", "src/game/generated/protocol.h", "src/game/tuning.h", "src/game/gamecore.cpp", network_header)
 
 client_link_other = {}
 client_depends = {}
@@ -195,7 +196,7 @@ function build(settings)
 	-- build the small libraries
 	wavpack = Compile(settings, Collect("src/engine/external/wavpack/*.c"))
 	pnglite = Compile(settings, Collect("src/engine/external/pnglite/*.c"))
-
+	
 	-- build game components
 	engine_settings = settings:Copy()
 	server_settings = engine_settings:Copy()
@@ -232,7 +233,7 @@ function build(settings)
 
 	versionserver = Compile(settings, Collect("src/versionsrv/*.cpp"))
 	masterserver = Compile(settings, Collect("src/mastersrv/*.cpp"))
-	game_shared = Compile(settings, Collect("src/game/*.cpp"), nethash, network_source)
+	game_shared = Compile(settings, Collect("src/game/*.cpp"), nethash, network_source, acchash)
 	game_client = Compile(settings, CollectRecursive("src/game/client/*.cpp"), client_content_source)
 	game_server = Compile(settings, CollectRecursive("src/game/server/*.cpp"), server_content_source)
 	game_editor = Compile(settings, Collect("src/game/editor/*.cpp"))
