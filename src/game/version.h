58c066d740c8a582d33e4939680312c8c49ef2e4
XXLTomate <XXLTomate@v93.eu>
2013-02-27 17:40:20 +0100

    raised version to 1.9a

--- src/game/version.h
+++ src/game/version.h
@@ -4,7 +4,7 @@
 #define GAME_VERSION_H
 #include "generated/nethash.cpp"
 #define DDRACE_VERSION "0.6, 1.15a"
-#define XXL_VERSION "1.9"
+#define XXL_VERSION "1.9a"
 #define GAME_VERSION DDRACE_VERSION ", " XXL_VERSION
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
 static const char GAME_RELEASE_VERSION[8] = {'0', '.', '6', '1', 0};
