1c6e5114f466f4becfc85ebc02040d74c714bf34
fstd <van.fisted@googlemail.com>
2015-01-17 03:17:57 +0100

    bumped version to 0.06

--- src/game/version.h
+++ src/game/version.h
@@ -3,7 +3,7 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 #include "generated/nethash.cpp"
-#define MOD_VERSION "0.05"
+#define MOD_VERSION "0.06"
 #define GAME_VERSION "0.6 trunk/" MOD_VERSION
 #define GAME_NETVERSION "0.6 " GAME_NETVERSION_HASH
 #define GAME_NETVERSION_CUST "0.6 " GAME_NETVERSION_HASH_CUST
