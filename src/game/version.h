bbdf017eca9aa9aa2d2a8e71e38648d3ccc73bf2
fstd <van.fisted@googlemail.com>
2015-01-17 15:25:50 +0100

    bump version to 0.08

--- src/game/version.h
+++ src/game/version.h
@@ -3,7 +3,7 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 #include "generated/nethash.cpp"
-#define MOD_VERSION "0.07"
+#define MOD_VERSION "0.08"
 #define GAME_VERSION "0.6.3/" MOD_VERSION
 #define GAME_NETVERSION "0.6 " GAME_NETVERSION_HASH
 #define GAME_NETVERSION_CUST "0.6 " GAME_NETVERSION_HASH_CUST
