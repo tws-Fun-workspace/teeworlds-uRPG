f3e31852fe98075ea7c7e14b9bb58d7772110187
fstd <van.fisted@googlemail.com>
2015-01-17 01:59:23 +0100

    add mod version

--- src/game/version.h
+++ src/game/version.h
@@ -3,7 +3,8 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 #include "generated/nethash.cpp"
-#define GAME_VERSION "0.6 trunk"
+#define MOD_VERSION "0.01"
+#define GAME_VERSION "0.6 trunk/" MOD_VERSION
 #define GAME_NETVERSION "0.6 " GAME_NETVERSION_HASH
 #define GAME_NETVERSION_CUST "0.6 " GAME_NETVERSION_HASH_CUST
 static const char GAME_RELEASE_VERSION[8] = {'0', '.', '6', '1', 0};
