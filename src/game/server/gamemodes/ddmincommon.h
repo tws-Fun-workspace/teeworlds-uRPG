72dd02cd0bb5b9d265f0a8432f24f56fda8f90d3
fstd <van.fisted@googlemail.com>
2015-01-19 22:59:48 +0100

    make it compile on win32 too

--- src/game/server/gamemodes/ddmincommon.h
+++ src/game/server/gamemodes/ddmincommon.h
@@ -5,34 +5,13 @@
 
 #define REFREEZE_INTERVAL_TICKS (Server()->TickSpeed()>>1)
 
-#define D(DF, DA...) fprintf(stderr,"%s:%i:%s() - " DF "\n",__FILE__,__LINE__,__func__,##DA)
-#define DVF "(%f, %f)"
-#define DVI "(%d, %d)"
-
-#define DV(V) (V).x, (V).y
-
-#define FLAGCOL_ELAST 0.5f
-
-#define TICK Server()->Tick()
-#define TICKSPEED Server()->TickSpeed()
-#define CFG(X) g_Config.m_Sv ## X
-
-#define MAX_BROADCAST 128
-
+#if defined(CONF_FAMILY_WINDOWS)
+ #define D(F, ...) dbg_msg("Flagdrag", "%s:%i:%s(): " F, __FILE__, __LINE__, \
+                                                            __FUNCTION__, __VA_ARGS__)
+#elif defined(CONF_FAMILY_UNIX)
+ #define D(F, ARGS...) dbg_msg("Flagdrag", "%s:%i:%s(): " F, __FILE__, __LINE__, \
 #endif
-#ifndef DDMINCOMMON_H
-#define DDMINCOMMON_H
 
-#include <cstdio>
-
-#define RACEBIT_TOUCH_FREEZE   0
-#define RACEBIT_TOUCH_UNFREEZE 1
-#define RACEBIT_TOUCH_START    2
-#define RACEBIT_TOUCH_FIN      3
-
-#define REFREEZE_INTERVAL_TICKS (Server()->TickSpeed()>>1)
-
-#define D(DF, DA...) fprintf(stderr,"%s:%i:%s() - " DF "\n",__FILE__,__LINE__,__func__,##DA)
 #define DVF "(%f, %f)"
 #define DVI "(%d, %d)"
 
