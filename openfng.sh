74cac2f08275fbfa15a533d59547374f17df0fb6
fstd <van.fisted@googlemail.com>
2015-09-17 01:16:17 +0200

    add openfng.sh. this obsoletes the linux binary releases.

---
+++ openfng.sh
@@ -0,0 +1,139 @@
+#!/bin/sh
+
+binary='openfng_srv'
+
+argv=
+bam='.bam_repo/bam/bam'
+git="$(command -v git)"
+prgnam="$(basename "$0")"
+
+Main()
+{
+	Deps
+	Update
+
+	[ -x "$binary" ] || BuildTW
+
+	Run
+	return $?
+}
+
+Say()
+{
+	printf '%s: %s\n' "$prgnam" "$1" >&2
+}
+
+Bomb()
+{
+	printf '%s: ERROR: %s\n' "$prgnam" "$1" >&2
+	if [ -z "$1" ]; then
+		printf '%s: ************************************************\n' "$prgnam" >&2
+		printf '%s: * !!!       PLEASE REPORT THIS ERROR       !!! *\n' "$prgnam" >&2
+		printf '%s: * !!!  Write a mail to van.fstd@gmail.com  !!! *\n' "$prgnam" >&2
+		printf '%s: * !!!  or report it on github if you have  !!! *\n' "$prgnam" >&2
+		printf '%s: * !!!  a github account:                   !!! *\n' "$prgnam" >&2
+		printf '%s: * https://github.com/fstd/teeworlds/issues/new *\n' "$prgnam" >&2
+		printf '%s: ************************************************\n' "$prgnam" >&2
+	fi
+	exit 1
+}
+
+Update()
+{
+	tmp=$(mktemp /tmp/openfng.sh.XXXXXXXX)
+	$git log -n1 >$tmp
+	Say "Pulling new commits, if any"
+	$git pull
+	if [ $($git log -n1 | diff -u $tmp - | wc -l) -gt 0 ]; then
+		# move away binary if we updated successfully so that it gets rebuilt
+		mv "$binary" "${binary}.old"
+		Say "Updated, we need to rebuild OpenFNG"
+	else
+		Say "Up to date."
+	fi
+}
+
+Deps()
+{
+	Say "Checking whether we have a toolchain..."
+	command -v cc >/dev/null \
+	    || command -v gcc >/dev/null \
+	    || command -v clang >/dev/null \
+	    || Bomb "Please install the toolchain (try 'sudo apt-get install build-essential')" silent
+
+	Say "Checking for git..."
+	[ -n "$git" ] || Bomb "Please install 'git' (try 'sudo apt-get install git')" silent
+
+	Say "Checking for bam..."
+	[ -x "$bam" ] || BuildBam
+}
+
+BuildBam()
+{
+	Say "We need to build bam."
+	mkdir -p .bam_repo
+	if ! grep -Fq .bam_repo .git/info/exclude; then
+		echo '.bam_repo' >> .git/info/exclude
+	fi
+	cd .bam_repo || Bomb "Failed to cd into .bam_repo"
+	Say "Cloning bam"
+	$git clone http://github.com/fstd/bam.git || Bomb "Failed to clone the bam repo"
+	cd bam || Bomb "Could not cd into .bam_repo/bam"
+
+	Say "Compiling bam"
+	./make_unix.sh
+	[ -x 'bam' ] || Bomb "Could not build bam"
+	bam="$(pwd)/bam"
+	cd ../..
+	Say "Bam successfully built"
+}
+
+BuildTW()
+{
+	Say "Building OpenFNG"
+	$bam config
+	$bam -c all #clean just to be sure
+	$bam server_release
+
+	[ -x "$binary" ] || Bomb "Failed to build OpenFNG"
+	Say "OpenFNG successfully built"
+}
+
+Run()
+{
+	Say "Running OpenFNG"
+	eval "set -- $argv"
+	./$binary "$@"
+	Say "OpenFNG terminated"
+	return $?
+}
+
+# ----This essentially is lstd-tiny.inc.sh (http://github.com/fstd/lstd) -------
+_lstd_tn_esc()
+{
+	_lstd_tn_input="$1"
+
+	printf "'"
+	while :; do
+		case "$_lstd_tn_input" in
+			\'*) printf "'\\\\''"              ;; # '\''
+			 ?*) printf '%c' "$_lstd_tn_input" ;;
+			 "") break                         ;;
+		esac
+		_lstd_tn_input="${_lstd_tn_input#?}"
+	done
+	printf "'"
+}
+
+list_mklist()
+{
+	while [ $# -gt 0 ]; do
+		_lstd_tn_esc "$1"
+		printf ' '
+		shift
+	done
+}
+# ---------------------------End of lstd-tiny.inc.sh ---------------------------
+
+Main "$@"
+exit $?
